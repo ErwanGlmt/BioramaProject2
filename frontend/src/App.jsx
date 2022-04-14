d4aa942c29fb0351782a2494d22a965abd84f55e
KevinFampou <fampoukevin3@gmail.com>
2022-04-14 09:47:36 +0200

    added css colors var and reset

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import Home from "@pages/Home";
-
+import React from "react";
+// import axios from "axios";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
+  /* const getData = () => {
+    axios
+      .get("https://opendata.agencebio.org/api/gouv/operateurs/")
+      .then((response) => console.log(response));
+  };
+  getData(); */
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <p>c</p>
     </div>
   );
 }
