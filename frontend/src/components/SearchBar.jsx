ec2c2da4f5cb68e93e45875886dc0d6e3687410d
AJL <jean-lamarre-alfred_student2022@wilder.school>
2022-04-16 11:26:45 +0200

    intégration css page SearchBar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
+import '../Search.css';
 
-const SearchBar = ()=> {
+const SearchBar = ({categorie, setCategorie, setVille})=> {
+    const handleChangeDept = (event)=> {
+        setVille(event.target.value)
+        //console.log(ville);
+    }
+
+    const handleChangeCat = (event)=> {
+        setCategorie(event.target.value)
+        console.log("ma categorie == " + categorie);
+    }
+
     return (
         <div>
             <form>
-                <label for="choice">Recherche par catégorie</label>
-                <select name="choice" id="choixCategorie" required >
-                    <option value="">--Choisissez une catégorie--</option>
-                    <option value="detaillant">Détaillants</option>
-                    <option value="grossiste">Grossiste</option>
-                    <option value="restoCafe">Restaurant / Cafés</option>
-                    <option value="gdDistribution">Grande Distribution</option>
-                    <option value="commercants">Commerçants / Artisans</option>
-                </select>
-                <input type="text" minlength="4" maxlength="15" required placeholder="Ville, un acteur ..." />
+                <div className="selectCat">
+                    <label htmlFor="categorie" >Recherche par catégorie</label>
+                    <select name="categorie" id="categorie" required onChange={(e)=> handleChangeCat(e)} >
+                        <option value="">--Choisissez une catégorie--</option>
+                        <option value='Grossistes'>Grossites</option>
+                        <option value='Magasins spécialisés'>Magasins Spécialisés</option>
+                        <option value='Vente aux consommateurs'>'{categorie}'</option>
+                        <option value='Artisans/commerçants'>Artisans/commerçants</option>
+                        <option value='restaurants'>Restaurants</option>
+                    </select>
+                </div>
+                
+                <div className="selectDept">   
+                    <label htmlFor="choixDept" >Département</label>
+                    <select name="choixDept" id="choixDept" required onChange={handleChangeDept} >
+                        <option value="">--Département--</option>
+                        <option value="loire">Loire-Atlantique</option>
+                        <option value="mayenne">Mayenne</option>
+                        <option value="idf">Ile-de-France</option>
+                        <option value="vilaine">Ille-et-Vilaine</option>
+                    </select>
+                </div>
+
+                <input type="text" minLength="4" maxLength="16" required placeholder="Ville de la recherche" />
                 <input type="submit" />
             </form>
         </div>
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import SearchBar from "@components/SearchBar";
 
-function Search() {
+import axios from 'axios';
+
+const Search = ()=> {
+  const [categorie, setCategorie] = useState("");
+  const [ville, setVille] = useState("");
+  const [data, setData] = useState([]);
+
+  const getData = ()=> {
+    axios
+      .get('https://opendata.agencebio.org/api/gouv/operateurs/')
+      .then(donnees => {
+        setData(donnees.data.items);
+        //console.log(data);
+      })
+  }
+  
+  useEffect(()=>{
+    getData();
+  }, [])
+
+  console.log(data)
+
   return (
     <div>
       <h1>Search Page</h1>
-      <SearchBar />
+      <SearchBar categorie={categorie} setCategorie={setCategorie} setVille={setVille} />
+      {
+        data.filter((donnees) => {
+         if(donnees.categories.length > 0 ) {
+           //&& donnees.categories[0].nom == categorie))) 
+           //console.log(donnees.categories[0].nom);
+           //console.log(categorie);
+           return donnees.categories[0].nom === categorie;
+          //donnees.categories.length > 0 && donnees.categories[0].nom == categorie))
+         }
+        })
+        .map((cat, index) => <p key={index}>{cat.raisonSociale}</p>)
+      }
     </div>
   );
 }
