6a480d842f6c43c84e949db8abe982aa8b92c62c
KevinFampou <fampoukevin3@gmail.com>
2022-04-20 16:02:29 +0200

    change classes to camelCase and removed commented code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-// import axios from "axios";
-
 import Header from "./Header";
 import Content from "./Content";
 import "./App.css";
 
 function App() {
-  /* const getData = () => {
-    axios
-      .get("https://opendata.agencebio.org/api/gouv/operateurs/")
-      .then((response) => console.log(response));
-  };
-  getData(); */
   return (
     <div className="App">
       <Header />
--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -11,34 +11,30 @@ function Header() {
   };
   return (
     <header>
-      <nav className={`navbar ${showLinks ? "show-nav" : null}`}>
-        <div className="navbar__logo">
+      <nav className={`navbar ${showLinks ? "showNav" : null}`}>
+        <div className="navbarLogo">
           <img className="logo" src="./src/assets/biorama2.png" alt="logo" />
         </div>
-        <ul className="navbar__links">
-          <li className="navbar__item slideInDown-1">
-            <Link onClick={handleShowLinks} className="navbar__link" to="/">
+        <ul className="navbarLinks">
+          <li className="navbarItem slideInDown-1">
+            <Link onClick={handleShowLinks} className="navbarLink" to="/">
               Accueil
             </Link>
           </li>
-          <li className="navbar__item slideInDown-2">
-            <Link
-              onClick={handleShowLinks}
-              className="navbar__link"
-              to="/Search"
-            >
+          <li className="navbarItem slideInDown-2">
+            <Link onClick={handleShowLinks} className="navbarLink" to="/Search">
               Trouver un acteur
             </Link>
           </li>
-          <li className="navbar__item slideInDown-3">
-            <Link onClick={handleShowLinks} className="navbar__link" to="/Info">
+          <li className="navbarItem slideInDown-3">
+            <Link onClick={handleShowLinks} className="navbarLink" to="/Info">
               Qui sommes-nous?
             </Link>
           </li>
-          <li className="navbar__item slideInDown-4">
+          <li className="navbarItem slideInDown-4">
             <Link
               onClick={handleShowLinks}
-              className="navbar__link"
+              className="navbarLink"
               to="/Contact"
             >
               Nous contacter
@@ -47,10 +43,10 @@ function Header() {
         </ul>
         <button
           type="button"
-          className="navbar__burger"
+          className="navbarBurger"
           onClick={handleShowLinks}
         >
-          <span className="burger-bar" />
+          <span className="burgerBar" />
         </button>
       </nav>
     </header>
