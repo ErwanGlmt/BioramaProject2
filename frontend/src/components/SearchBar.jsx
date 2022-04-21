c3bf0b63181d305ebe9f8fcf22fb2af8bcb282a2
AJL <jean-lamarre-alfred_student2022@wilder.school>
2022-04-21 09:45:03 +0200

    page search, filtrage par catégorie ok

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,10 +1,17 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import '../Search.css';
+import Card from './Card';
+import axios from 'axios';
 
-const SearchBar = ({categorie, setCategorie, setVille})=> {
-    const handleChangeDept = (event)=> {
+const SearchBar = ()=> {
+  const [categorie, setCategorie] = useState("");
+  const [ville, setVille] = useState("");
+  const [data, setData] = useState([]);
+
+    const handleChangeCity = (event)=> {
         setVille(event.target.value)
-        //console.log(ville);
+        //console.log("ma ville == " + ville);
     }
 
     const handleChangeCat = (event)=> {
@@ -12,36 +19,51 @@ const SearchBar = ({categorie, setCategorie, setVille})=> {
         console.log("ma categorie == " + categorie);
     }
 
-    return (
-        <div>
-            <form>
-                <div className="selectCat">
-                    <label htmlFor="categorie" >Recherche par catégorie</label>
-                    <select name="categorie" id="categorie" required onChange={(e)=> handleChangeCat(e)} >
-                        <option value="">--Choisissez une catégorie--</option>
-                        <option value='Grossistes'>Grossites</option>
-                        <option value='Magasins spécialisés'>Magasins Spécialisés</option>
-                        <option value='Vente aux consommateurs'>'{categorie}'</option>
-                        <option value='Artisans/commerçants'>Artisans/commerçants</option>
-                        <option value='restaurants'>Restaurants</option>
-                    </select>
-                </div>
-                
-                <div className="selectDept">   
-                    <label htmlFor="choixDept" >Département</label>
-                    <select name="choixDept" id="choixDept" required onChange={handleChangeDept} >
-                        <option value="">--Département--</option>
-                        <option value="loire">Loire-Atlantique</option>
-                        <option value="mayenne">Mayenne</option>
-                        <option value="idf">Ile-de-France</option>
-                        <option value="vilaine">Ille-et-Vilaine</option>
-                    </select>
-                </div>
+    const getData = ()=> {
+        axios
+        .get('https://opendata.agencebio.org/api/gouv/operateurs/')
+        .then(donnees => {
+            setData(donnees.data.items);
+        })
+    }
+    
+    useEffect(()=>{
+        getData();
+    }, [])
 
-                <input type="text" minLength="4" maxLength="16" required placeholder="Ville de la recherche" />
-                <input type="submit" />
-            </form>
-        </div>
+    console.log(data)
+    return (
+        <>
+            <div className='container'>
+                <form>
+                    <div className="selectCat">
+                        <select name="categorie" id="categorie" required onChange={(e)=> handleChangeCat(e)} >
+                            <option value="">-- Choisissez une catégorie --</option>
+                            <option value='Grossistes'>Grossistes</option>
+                            <option value='Magasins spécialisés'>Magasins Spécialisés</option>
+                            <option value='Grandes surfaces généralistes'>Grandes surfaces généralistes</option>
+                            <option value='Artisans/commerçants'>Artisans / commerçants</option>
+                        </select>
+                    </div>
+
+                    <input type="text" minLength="4" maxLength="16" required placeholder="Ville de la recherche" onChange={(e)=> handleChangeCity(e)} />
+                    <input type="submit" value="Rechercher" /* onSubmit ={(e)=> }*/ />
+                </form>
+            </div>
+            
+            {
+                data.filter((donnees) => {
+                    if(donnees.categories.length > 0 ) {
+                        if (categorie === "" ) return donnees.categories[0];
+                        if (ville !== "" ) return donnees.adressesOperateurs[0].ville == ville;
+                        else return donnees.categories[0].nom == categorie;
+                    }})
+                    .map((cat, index) => {
+                        return <Card {...cat} key={index} />
+                    }
+                )
+            }
+        </>
     );
 }
 
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,47 +1,13 @@
-
-import React, { useState, useEffect } from "react";
+import '../Search.css';
 import SearchBar from "@components/SearchBar";
 
-import axios from 'axios';
-
-const Search = ()=> {
-  const [categorie, setCategorie] = useState("");
-  const [ville, setVille] = useState("");
-  const [data, setData] = useState([]);
-
-  const getData = ()=> {
-    axios
-      .get('https://opendata.agencebio.org/api/gouv/operateurs/')
-      .then(donnees => {
-        setData(donnees.data.items);
-        //console.log(data);
-      })
-  }
-  
-  useEffect(()=>{
-    getData();
-  }, [])
-
-  console.log(data)
-
+const Search = () =>{
   return (
-    <div>
-      <h1>Search Page</h1>
-      <SearchBar categorie={categorie} setCategorie={setCategorie} setVille={setVille} />
-      {
-        data.filter((donnees) => {
-         if(donnees.categories.length > 0 ) {
-           //&& donnees.categories[0].nom == categorie))) 
-           //console.log(donnees.categories[0].nom);
-           //console.log(categorie);
-           return donnees.categories[0].nom === categorie;
-          //donnees.categories.length > 0 && donnees.categories[0].nom == categorie))
-         }
-        })
-        .map((cat, index) => <p key={index}>{cat.raisonSociale}</p>)
-      }
+    <div className="div-recherche">
+      <h1>Recherche</h1>
+      <SearchBar />
     </div>
   );
-}
-
+};
+ 
 export default Search;
