69bde75216ad5d191612b52eaf388b59d3babd6a
KevinFampou <fampoukevin3@gmail.com>
2022-04-28 10:47:06 +0200

    contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import "../App.css";
+import "../styles/Contact.css";
 
 function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <h1>Contact Page</h1>
+      <div className="title">
+        <h1>Nous contacter</h1>
+      </div>
+      <div className="paragraph">
+        <p>
+          Si vous avez une demande particulière ou voulez souhaitez etre
+          référencé sur notre site, voici un petit formulaire pour nous joindre.
+          Nous vous répondrons dans les plus brefs délais.
+        </p>
+      </div>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            minLength={8}
+            placeholder="Votre nom ou raison sociale"
+            required
+          />
+          <input
+            type="text"
+            min={5}
+            placeholder="Objet de la demande"
+            required
+          />
+          <input type="email" placeholder="exemple@email.fr" required />
+          <textarea
+            name="message"
+            minLength="16"
+            cols="30"
+            rows="10"
+            required
+          />
+          <input className="submit" type="submit" value="Envoyer" />
+        </form>
+      </div>
     </div>
   );
 }
