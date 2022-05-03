ced2b069be0c074ac60bbbf5355338c10cde3f64
KevinFampou <fampoukevin3@gmail.com>
2022-05-03 10:54:33 +0200

    active links + contact page done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -9,10 +9,10 @@ function Contact() {
   return (
     <div>
       <div className="title">
-        <h1>Nous contacter</h1>
+        <h1 className="contactTitle">Nous contacter</h1>
       </div>
       <div className="paragraph">
-        <p>
+        <p className="pContact">
           Si vous avez une demande particulière ou voulez souhaitez etre
           référencé sur notre site, voici un petit formulaire pour nous joindre.
           Nous vous répondrons dans les plus brefs délais.
@@ -21,18 +21,25 @@ function Contact() {
       <div className="form">
         <form onSubmit={handleSubmit}>
           <input
+            className="inputContact"
             type="text"
             minLength={8}
             placeholder="Votre nom ou raison sociale"
             required
           />
           <input
+            className="inputContact"
             type="text"
             min={5}
             placeholder="Objet de la demande"
             required
           />
-          <input type="email" placeholder="exemple@email.fr" required />
+          <input
+            className="inputContact"
+            type="email"
+            placeholder="exemple@email.fr"
+            required
+          />
           <textarea
             name="message"
             minLength="16"
