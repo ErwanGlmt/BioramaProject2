e59bb1ecdd4bc5900d674d462141ae8b4388dabd
ErwanGlmt <erwanguillemot7@gmail.com>
2022-05-03 15:00:31 +0200

    Finalisation de la page information avec les deux versions fonctionnelles desktop et mobile

--- frontend/src/components/Presentation.jsx
+++ frontend/src/components/Presentation.jsx
@@ -5,7 +5,7 @@ function Presentation() {
   return (
     <div className="presentation-text">
       <h1>Qui sommes-nous?</h1>
-      <p>
+      <p id="prestextdesktop">
         Suivant une formation Développeur Web intensive de 5 mois au sein de la
         Wild Code School. Notre équipe en est venue à collaborer au sein du
         Projet 2 de la formation. Une création de site internet qui devait se
@@ -18,6 +18,28 @@ function Presentation() {
         ou tout simplement de permettre au particulier de s&apos;informer des
         acteurs proches de chez eux.
       </p>
+      <p id="prestextmobile">
+        Suivant une formation Développeur Web intensive de 5 mois au sein de la
+        Wild Code School. Notre équipe en est venue à collaborer au sein du
+        Projet 2 de la formation. Une création de site internet qui devait se
+        faire sous React mais aussi avec l&apos;utilisation d&apos;une API
+        publique. Nous voulions que notre site ne soit pas seulement une
+        application fonctionnelle mais aussi une application qui apporte du sens
+        et de l&apos;utilité.
+      </p>
+      <div className="photopresentation">
+        <img
+          src="./src/assets/photoequipe.jpg"
+          className="photoequipe"
+          alt="Notre team"
+        />
+        <div className="membreequipe">
+          <p>Séverine</p>
+          <p>Lamarre</p>
+          <p>Kévin</p>
+          <p>Erwan</p>
+        </div>
+      </div>
     </div>
   );
 }
