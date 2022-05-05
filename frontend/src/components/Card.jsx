1e1b45ba2dc0d4db374f14f093baec48d16f2986
AJL <jean-lamarre-alfred_student2022@wilder.school>
2022-05-05 11:12:15 +0200

    fixation Card.css et mise en place carte descriptive (fiche détaillée de chaque acteur)

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,8 +2,43 @@ import React from "react";
 import "../styles/Card.css";
 import PropTypes from "prop-types";
 
-function Card({ lieu, ville, codePostal, categories, raisonSociale }) {
-  return (
+function Card({
+  lieu,
+  ville,
+  codePostal,
+  categories,
+  raisonSociale,
+  productions,
+  gerant,
+  dateSuspension,
+  cardId,
+  setCardId,
+}) {
+  return !cardId ? (
+    <div className="div-card-details div-card">
+      <div className="div-image">
+        <img src="./src/assets/slide3.jpg" alt="distributeur" />
+      </div>
+      <div>
+        <h2>{raisonSociale}</h2>
+        <p>{categories}</p>
+        <address>
+          {lieu}
+          <br />
+          {codePostal} - {ville}
+          <br />
+        </address>
+        <p>Gérant : {gerant}</p>
+        <p>Productions:</p>
+        <ul>
+          {productions.map((produit) => (
+            <li key={produit.id}>{produit.nom}</li>
+          ))}
+        </ul>
+        <p>{dateSuspension}</p>
+      </div>
+    </div>
+  ) : (
     <div className="div-card">
       <div className="div-image">
         <img src="./src/assets/slide3.jpg" alt="distributeur" />
@@ -17,6 +52,13 @@ function Card({ lieu, ville, codePostal, categories, raisonSociale }) {
           {codePostal} - {ville}
           <br />
         </address>
+        <button
+          type="button"
+          value={cardId}
+          onClick={(e) => setCardId(Number(e.target.value))}
+        >
+          Plus de détails ...
+        </button>
       </div>
     </div>
   );
@@ -28,6 +70,11 @@ Card.propTypes = {
   ville: PropTypes.string,
   categories: PropTypes.string,
   raisonSociale: PropTypes.string,
+  productions: PropTypes.arrayOf(PropTypes.string),
+  gerant: PropTypes.string,
+  dateSuspension: PropTypes.string,
+  cardId: PropTypes.number,
+  setCardId: PropTypes.func,
 };
 
 Card.defaultProps = {
@@ -36,6 +83,11 @@ Card.defaultProps = {
   codePostal: "",
   categories: "NoCategorie",
   raisonSociale: "NoRaisonSociale",
+  productions: [],
+  gerant: "",
+  dateSuspension: "",
+  cardId: "",
+  setCardId: "",
 };
 
 export default Card;
